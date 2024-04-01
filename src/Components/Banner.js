dcbcf172885274df1bad20ae61a1bf488c92fd97
Abdulwasim-S <abdulwasimsmech@gmail.com>
2024-04-01 17:36:01 +0530

    update

--- src/Components/Banner.js
+++ src/Components/Banner.js
@@ -4,17 +4,23 @@ import { LazyLoadImage } from "react-lazy-load-image-component";
 
 const Banner = () => {
   return (
-    <><div className="banner-page">
-      <LazyLoadImage
-        className="profile"
-        alt="profile"
-        src="https://drive.google.com/uc?export=view&id=1egyFxCgosf-mH0WG-kqNksusOQOOvhM_"
-        effect="blur" />
-    </div><div className="about-page">
+    <>
+      <div className="banner-page">
+        <LazyLoadImage
+          className="profile"
+          alt="profile"
+          src="https://drive.google.com/thumbnail?export=view&id=1egyFxCgosf-mH0WG-kqNksusOQOOvhM_&sz=w10000"
+          effect="blur"
+        />
+      </div>
+      <div className="about-page">
         <h3>HI,I'm Abdulwasim</h3>
         <h1>Full Stack Developer</h1>
-        <Button color="info" href="#contact">Contact Me</Button>
-      </div></>
+        <Button color="info" href="#contact">
+          Contact Me
+        </Button>
+      </div>
+    </>
   );
 };
 
